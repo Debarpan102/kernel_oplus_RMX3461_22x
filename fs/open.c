7489fd12e78e6fae895228c6c268e1ba4564620f
Debarpan102 <debarpanhalder8@gmail.com>
2025-01-09 17:20:23 +0530

    fs:open.c: Fix error

--- fs/open.c
+++ fs/open.c
@@ -361,10 +361,6 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 
-	#ifdef CONFIG_KSU
-	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
-	#endif
-
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
 	if (susfs_is_sus_su_hooks_enabled) {
 		ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
